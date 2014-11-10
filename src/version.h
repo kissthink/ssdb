cdf6c3d6a7f165d1d30b391a7df0e05765febdd0
ideawu <ideawu@local>
2014-11-10 10:35:05 +0800

    add copyright msg

--- src/version.h
+++ src/version.h
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
 #ifndef SSDB_DEPS_H
 #ifndef SSDB_VERSION
 #define SSDB_VERSION "1.6.8.4"
