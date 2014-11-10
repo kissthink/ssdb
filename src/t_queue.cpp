af7c4b0e9ce6e423816bf066ac9ee812b2694856
ideawu <ideawu@local>
2014-11-10 10:29:54 +0800

    add copyright msg

--- src/t_queue.cpp
+++ src/t_queue.cpp
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
 #include "t_queue.h"
 #include "ssdb.h"
 #include "leveldb/write_batch.h"
